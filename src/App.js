3cf388cd6b4dbdb7783a3046f112478ec2806c27
Andy Shora <andyshora@gmail.com>
2022-07-20 10:33:51 +0100

    Navigation working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,116 @@
-import * as React from 'react';
+import { useState, useMemo } from 'react';
+import IconButton from '@mui/material/IconButton';
+import NextIcon from '@mui/icons-material/ArrowForwardIos';
+import PrevIcon from '@mui/icons-material/ArrowBackIos';
+
 import Tooltip from './components/Tooltip';
 import story from "./content/story.json"
+import styled from 'styled-components';
 
+const NavWrap = styled.nav`
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  padding: 1rem;
+`
+
+const DebugWrap = styled.aside`
+  width: 100%;
+  position: fixed;
+  top: 0;
+  right: 0;
+  padding: 1rem;
+  color: blue;
+  text-align: right;
+`
 
 export default function App() {
+
+  const [activeKey, setActiveKey] = useState("")
+  const [activeSceneIndex, setActiveSceneIndex] = useState(0)
+  const [activeAnnotationIndex, setActiveAnnotationIndex] = useState(0)
+  const isOnLastAnnotationOfScene = activeAnnotationIndex === story.scenes[story.scenes.length - 1].annotations.length - 1
+
+  const isAtStart = !activeSceneIndex && !activeAnnotationIndex
+  const isAtEnd = activeSceneIndex === story.scenes.length - 1 && isOnLastAnnotationOfScene
+
+  const viewXY = useMemo(() => {
+    return [
+      story.scenes[activeSceneIndex].view_x,
+      story.scenes[activeSceneIndex].view_y
+    ]
+  }, [activeSceneIndex])
+
+  function handleAreaClick(e) {
+    const key = e.target.getAttribute("data-area-key")
+
+    if (key) {
+      setActiveKey(key)
+      setActiveAnnotationIndex(0)
+    }
+
+    const _activeSceneIndex = story.scenes.findIndex(s => s.key === key)
+    if (_activeSceneIndex > -1) {
+      setActiveSceneIndex(_activeSceneIndex)
+    }
+  }
+  function handlePrevClick() {
+    
+    if (isAtStart) { return }
+    if (!activeAnnotationIndex) {
+      // first annotation, so go to last annotation of prev scene
+      setActiveSceneIndex(s => s - 1)
+      setActiveAnnotationIndex(story.scenes[activeSceneIndex - 1].annotations.length - 1)
+    } else {
+      setActiveAnnotationIndex(i => i - 1)
+    }
+
+  }
+  function handleNextClick() {
+    if (isAtEnd) {
+      // go to start
+      setActiveSceneIndex(0)
+      setActiveAnnotationIndex(0)
+      return
+    }
+
+    if (isOnLastAnnotationOfScene) {
+      // next scene available, last annotation
+      setActiveSceneIndex(s => s + 1)
+      setActiveAnnotationIndex(0)
+      return
+    } else {
+      // same scene, next annotation
+      setActiveAnnotationIndex(i => i + 1)
+    }
+    
+  }
+
   return (
     <div>
+      
       <div>
         {story.scenes.map((scene, i) => 
           <div key={`scene-${scene.key}`}>
-            {scene.annotations.map((annot, j) => <Tooltip key={`scene-${scene.key}--annot-${j}`} {...annot} />)}
+            {scene.annotations.map((annot, j) => <Tooltip key={`scene-${scene.key}--annot-${j}`} {...annot} isActive={activeSceneIndex === i && activeAnnotationIndex === j} />)}
           </div>
         )}
       </div>
       <div>
-        <img src={process.env.PUBLIC_URL + '/images/fab36.png'} useMap="#scenemap" />
+        <img src={process.env.PUBLIC_URL + '/images/fab36.png'} useMap="#scenemap" alt="" />
         <map name="scenemap">
-          <area shape="rect" coords="0,0,100,100" alt="Intelligence" onClick={() => alert(1)} />
+          <area shape="rect" coords="0,0,100,100" alt="Intelligence" onClick={handleAreaClick} data-area-key="intelligence" />
         </map>
       </div>
+      <NavWrap>
+        <IconButton aria-label="Backward" onClick={handlePrevClick} disabled={isAtStart}>
+          <PrevIcon />
+        </IconButton>
+        <IconButton aria-label="Forward" onClick={handleNextClick}>
+          <NextIcon />
+        </IconButton>
+      </NavWrap>
+      <DebugWrap>viewXY: {viewXY.join(',')}, activeKey: {activeKey}, activeSceneIndex: {activeSceneIndex}, activeAnnotationIndex: {activeAnnotationIndex}, isAtStart: {isAtStart ? "YES" : ""}, isAtEnd: {isAtEnd ? "YES" : ""}</DebugWrap>
     </div>
   )
 }
--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
+import styled from 'styled-components';
 import { Typography } from '@mui/material';
 
 const TOOLTIP_DEFAULT_X = 40
 const TOOLTIP_DEFAULT_Y = 30
+const TOOLTIP_WIDTH = 240
+
+const TooltipWrap = styled.aside`
+    padding: 0.5rem 0.8rem;
+    user-select: none;
+    background: linear-gradient(0deg, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0.5));
+    border-radius: 3px;
+    width: ${TOOLTIP_WIDTH}px;
+`
 
 export default function Tooltip({
     tooltip_x = 0,
@@ -11,16 +20,21 @@ export default function Tooltip({
     tooltip_title = "Title",
     tooltip_desc = "",
     tooltip_offset_x = TOOLTIP_DEFAULT_X,
-    tooltip_offset_y = TOOLTIP_DEFAULT_Y
+    tooltip_offset_y = TOOLTIP_DEFAULT_Y,
+    tooltip_dir = "",
+    isActive = false
 }) {
+const onLeft = tooltip_dir === "bl" || tooltip_dir === "tl"
+const offset_x = onLeft ? -TOOLTIP_WIDTH : 0
   return (
-    <aside style={{
-        left: tooltip_x,
+    <TooltipWrap style={{
+        left: tooltip_x + offset_x,
         top: tooltip_y,
-        position: "absolute"
+        position: "absolute",
+        border: isActive ? "4px solid lime" : "none" 
     }}>
-      <Typography variant="h3">{tooltip_title}</Typography>
+      <Typography variant="h5">{tooltip_title}</Typography>
       {tooltip_desc ? <Typography>{tooltip_desc}</Typography> : ""}
-    </aside>
+    </TooltipWrap>
   )
 }
